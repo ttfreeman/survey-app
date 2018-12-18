a952eaf3d235a22dafae7aab10140f8ee6867422
Tony Freeman <ttfreeman@gmail.com>
2018-12-18 09:42:47 -0700

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ key: "hello world" });
+  res.send({ hi: "my friend" });
 });
 
 const PORT = process.env.PORT || 5000;
