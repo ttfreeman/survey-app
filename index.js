3d95519185c37fd255eca720545fd452d99a7ae9
Tony Freeman <ttfreeman@gmail.com>
2018-12-18 13:52:33 -0700

    third commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "my friend" });
+  res.send({ hi: "my new friend" });
 });
 
 const PORT = process.env.PORT || 5000;
